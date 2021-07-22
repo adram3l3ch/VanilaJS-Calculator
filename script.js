e6dab3d6ffa94533ac55761a1f5f3a4b8b68faf2
adramelech <adramelech.psd@gmail.com>
2021-07-22 16:58:50 +0530

    final-commit

---
+++ script.js
@@ -0,0 +1,126 @@
+const number = document.querySelectorAll("[data-number]");
+const operation = document.querySelectorAll("[data-operation]");
+const ac = document.querySelector("[data-ac]");
+const deletebtn = document.querySelector("[data-del]");
+const equals = document.querySelector("[data-equals]");
+const topbtn = document.querySelector("[data-top]");
+const btmbtn = document.querySelector("[data-bottom]");
+
+// -------------------Functions---------------------------
+
+const updateDisplay = (num) => {
+  let regexp = /[*-/+]/;
+  let arr = topbtn.innerText.split("");
+  if (/[.]/.test(btmbtn.innerText) && num.innerText === ".") {
+  } else {
+    if (regexp.test(topbtn.innerText) && !regexp.test(arr[arr.length - 1])) {
+      topbtn.innerText = "";
+      btmbtn.innerText += num.innerText;
+    } else {
+      if (btmbtn.innerText === "0") {
+        btmbtn.innerText = num.innerText;
+      } else {
+        btmbtn.innerText += num.innerText;
+      }
+    }
+  }
+};
+
+const operations = (ope) => {
+  if (btmbtn.innerText === "0" && btmbtn.innerText.length === 1) {
+  } else {
+    if (topbtn.innerText === "") {
+      topbtn.innerText += btmbtn.innerText + ope.innerText;
+      btmbtn.innerText = "0";
+    } else {
+      find();
+      topbtn.innerText = "";
+      topbtn.innerText += btmbtn.innerText + ope.innerText;
+      btmbtn.innerText = "0";
+    }
+  }
+};
+
+const deletekey = () => {
+  if (
+    btmbtn.innerText !== "0" ||
+    btmbtn.innerText.length !== 1 ||
+    topbtn.innerText
+  ) {
+    let arr = btmbtn.innerText.split("");
+    let regexp = /[*-/+]/;
+    if (arr.length === 1 && arr[arr.length - 1] === "0") {
+      arr = topbtn.innerText.split("");
+      if (regexp.test(arr[arr.length - 1])) {
+        arr.pop();
+        topbtn.innerText = "";
+        btmbtn.innerText = arr.join("");
+      } else {
+        btmbtn.innerText = arr[arr.length - 1];
+        arr.pop();
+        topbtn.innerText = arr.join("");
+      }
+    } else {
+      arr.pop();
+      btmbtn.innerText = arr.join("");
+    }
+  }
+  if (btmbtn.innerText === "") {
+    btmbtn.innerText = "0";
+  }
+};
+
+const find = () => {
+  if (
+    topbtn.innerText === "" ||
+    (btmbtn.innerText === "0" && btmbtn.innerText.length === 1)
+  ) {
+  } else {
+    let regexp = /[*-/+]/;
+    if (regexp.test(topbtn.innerText[topbtn.innerText.length - 1])) {
+      let arr = topbtn.innerText.split("");
+      let operation = arr[arr.length - 1];
+      arr.pop();
+      let firstNum = Number(arr.join(""));
+      let secondNum = Number(btmbtn.innerText);
+      topbtn.innerText += btmbtn.innerText;
+      switch (operation) {
+        case "/":
+          btmbtn.innerText = firstNum / secondNum;
+          break;
+        case "*":
+          btmbtn.innerText = firstNum * secondNum;
+          break;
+        case "+":
+          btmbtn.innerText = firstNum + secondNum;
+          break;
+        case "-":
+          btmbtn.innerText = firstNum - secondNum;
+          break;
+      }
+    }
+  }
+};
+
+// ---------------------------EvenListeners-------------------------------
+
+number.forEach((num) => {
+  num.addEventListener("click", () => {
+    updateDisplay(num);
+  });
+});
+
+operation.forEach((ope) => {
+  ope.addEventListener("click", () => {
+    operations(ope);
+  });
+});
+
+ac.addEventListener("click", () => {
+  topbtn.innerText = "";
+  btmbtn.innerText = "0";
+});
+
+deletebtn.addEventListener("click", deletekey);
+
+equals.addEventListener("click", find);
